aaa6c787419df77d8a0c473c7ed21158bd017895
Archit Pandya <architpandya@yahoo.com>
2025-12-30 17:44:27 -0500

    Modified constants.py

--- networksecurity/components/constants.py
+++ networksecurity/components/constants.py
@@ -1,12 +1,15 @@
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 import os
 #----------------------------------------------------------
-load_dotenv()
+load_dotenv(find_dotenv())
 ROOT_DIR = Path.cwd()
 network_data_dir = ROOT_DIR / 'network_data'
 network_data_file_and_path = network_data_dir / os.getenv("load_file_to_mongo")
+MONGO_DB_SUFFIX = os.getenv("MONGO-DB-URI-SUFFIX")
 MONGO_DB_URI = os.getenv("MONGO-DB-URI")
 #----------------------------------------------------------
 if __name__ == "__main__":
     print(f"Network Data Path: {network_data_file_and_path}")
+    print(f"MongoDB Suffix: {MONGO_DB_SUFFIX}")
+    print(f"MongoDB URI: {MONGO_DB_URI}")
